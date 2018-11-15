cdaffdf701e8a675b9ecf3eda636b704a3c68514
Phil Jones <interstar@gmail.com>
2018-11-15 11:14:40 -0200

    added gitlab url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fsquery "0.1.0-SNAPSHOT"
   :description "FSQuery : Make the File System like JQuery. "
-  :url ""
+  :url "https://gitlab.com/interstar/FSQuery-CLJ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
